a0a3a97ef21524e12167bf2f2491b3416306eed9
Dong-Jae Oh <93889207+djdongjae@users.noreply.github.com>
2022-11-08 20:23:52 +0900

    Add files via upload

--- 02-DDL.sql
+++ 02-DDL.sql
@@ -22,5 +22,27 @@ orderdate   date,
 primary key(orderid),
 foreign key(custid) references newcustomer(custid) on delete cascade);
 
+--질의3-37: NewBook 테이블에 VARCHAR2(13)의 자료형을 가진 isbn 속성을 추가하시오.
+alter table newbook add isbn varchar2(13);
 
+--질의3-38: NewBook 테이블의 isbn 속성의 데이터 타입을 NUMBER형으로 변경하시오.
+alter table newbook modify isbn number;
 
+--질의3-39: NewBook 테이블의 isbn 속성을 삭제하시오.
+alter table newbook drop column isbn;
+
+--질의3-40: NewBook 테이블의 bookid 속성에 NOT NULL 제약조건을 적용하시오.
+alter table newbook modify bookid number not null;
+
+--질의3-41: NewBook 테이블의 bookid 속성을 기본키로 변경하시오.
+alter table newbook add primary key(bookid);
+
+--질의3-42: NewBook 테이블을 삭제하시오.
+drop table newbook;
+
+--질의3-43: NewCustomer 테이블을 삭제하시오. 만약 삭제가 거절된다면 원인을 파악하고 관련된 테이블을 같이 삭제하시오.
+--(NewOrders 테이블이 NewCustomer를 참조하고 있는 상태이다.)
+drop table newcustomer;
+
+drop table neworders;
+drop table newcustomer;
