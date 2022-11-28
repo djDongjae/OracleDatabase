33928b10b41577df78e9b4ab93ef1061b657b469
Dong-Jae Oh <isf1999@naver.com>
2022-11-28 14:44:46 +0900

    .

---
+++ 4장_SQL고급_연습문제(HR).sql
@@ -0,0 +1,10 @@
+--1. 사원의 성과 함께 급여와 commission을 출력하라. 급여는 천의 자리 이상만 출력(반올림)하고, commission은 소수 첫째자리까지만 출력(반올림)하라.
+select last_name, round(salary, -3), round(commission_pct, 1)
+from employees;
+
+--2. 60번 부서에 속한 사원의 성을 대문자로 출력하라.
+select upper(last_name)
+from employees
+where department_id = 60;
+
+--3. JOB_HISTORY 테이블을 이용하여, 각 사원 번호와 해당 사원의 종료날짜, 종료한 날짜의 2개월 후 달의 마지막 날짜를 출력하라.
