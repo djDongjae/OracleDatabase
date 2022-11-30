01d8721e801b3cdad89115030daecda1170bc7c1
Dong-Jae Oh <isf1999@naver.com>
2022-11-30 22:04:08 +0900

    .

--- 4장_SQL고급_연습문제(HR).sql
+++ 4장_SQL고급_연습문제(HR).sql
@@ -12,26 +12,29 @@ select employee_id, end_date, last_day(add_months(end_date, 2))
 from job_history;
 
 --4. 사원번호가 200번대인 사원의 사원번호, 고용날짜를 출력하라. 날짜는 연. 월. 일. 요일을 출력한다.
-select employee_id, to_char(hire_date, 'yyyy.mm.dd,dy')
+select employee_id, to_char(hire_date, 'yyyy.mm.dd.dy')
 from employees
 where employee_id >= 200 and employee_id < 300;
 
 --5. 2005.03.01이전에 고용된 사원의 성, 고용일을 출력하라. 이 질의는 NLS_LANG 변수가 어떤 변수로 설정되어도 작동할 수 있도록 하라.
-select nvl(last_name), nvl(hire_date)
+select last_name, to_char(hire_date, 'yyyy-mm-dd') "고용일"
 from employees
-where to_date();
+where hire_date < to_date('20050301', 'yyyymmdd');
 
 /*
 6. 연봉은 12개월 급여에 commission_pct만큼을 추가하여 지급된다.
 commission이 NULL인 경우 0으로 간주하여 연봉을 계산한 후, 사원의
 사원번호와 연봉(YEAR_SAL)을 연봉이 커지는 순서로 출력하라.
 */
-select employee_id, (salary*12) + (salary * 12 * nvl(commission_pct, 0)) as "year_sel"
+select employee_id, (salary*12) + (salary * 12 * nvl(commission_pct, 0)) as "year_salary"
 from employees
-order by "year_sel";
+order by "year_salary" asc;
 
 --7. commission이 설정되어 있는 사원의 사원 번호와 commission_pct를, commission_pct가 가장 큰 사원부터 출력하라.
-select employee_id, commis
+select employee_id, commission_pct
+from employees
+where commission_pct is not null
+order by commission_pct desc;
 
 --view 연습
 
@@ -40,17 +43,41 @@ select employee_id, commis
 EMPLOYEE라는 컬럼으로 표시), 부서 번호를 가지는 뷰 EMPLOYEE_VU를 생
 성하라.
 */
+create view employee_vu
+as select employee_id,
+first_name ||' '|| last_name as "EMPLOYEE",
+department_id
+from employees;
 
 --2. EMPLOYEE_VU를 이용하여, 사원이름과 해당 사원이 소속된 부서를 출력하라.
+select ev.employee, d.department_name
+from employee_vu ev, departments d
+where ev.department_id = d.department_id;
 
 --3. 부서이름, 해당 부서의 평균 급여(컬럼 이름은 AVG_SAL), 부서장 이름을 갖는 뷰인 DEPARTMENT_VU를 생성하라. 뷰를 통해 내용을 확인하라.
+create view department_vu
+as select d.department_name, round(avg(salary), 2) as "avg_sal"
+from employees e, departments d
+where e.department_id =  d.department_id
+group by d.department_name;
 
 --4. 80번 부서에 속한 사원만 보이도록 1번에서 정의된 뷰를 수정하라.
+create view employee_vu_2
+as select employee_id, first_name ||' '|| last_name as "EMPLOYEE", department_id
+from employees
+where department_id = 80;
 
 --5. 사원의 성(last_name)으로 검색되는 경우가 많았다. 검색의 효율을 높일 수 있는 명령을 작성하라.
+create index ix
+on employees(last_name);
 
 --6. WHERE절에서 사원번호와 부서번호가 같이 검색되는 경우가 많다. 검색의 효율을 높일 수 있는 명령을 작성하라.
+create index emp_id_with_department_id
+on employees(employee_id, department_id);
 
 --7. salary가 where절에서 검색될 때는 항상 salary가 큰 사원부터 작은 사원 순서로 검색되는 경우가 많다고 가정하자. 이를 반영하여 인덱스를 설정하라.
+create index ix_salary 
+on employees (salary desc);
 
 --8. 7에서 설정된 인덱스를 삭제하라.
+drop index ix_salary;
--- SQL기초2차강의실습_201914089_2ndReading.sql
+++ SQL기초2차강의실습_201914089_2ndReading.sql
@@ -44,3 +44,21 @@ where salary >= 4000;
 --3.3.1 Outer Join 연습
 
 --1. 사원이 소속되지 않은 부서를 포함하여, 부서이름과 해당 부서에 소속된 사원 이름을 출력하라.
+select d.department_name, e.last_name
+from departments d left outer join employees e
+on d.department_id = e.department_id;
+
+select d.department_name, e.last_name
+from departments d left outer join employees e
+using (department_id);
+
+/*
+2. 업무가 배정되지 않은 사원과 사원이 배정되지 않은 업무를 포함하여,
+모든 사원 이름(first_name)과 업무 이름(job_title)을 출력하라.
+*/
+select e.first_name, j.job_title
+from employees e full outer join jobs j
+using (job_id);
+
+-- 3. 부서가 배치되지 않은 위치(locations)를 포함하여 모든 위치의 도시이름(city)와 부서이름을 출력하라.
+
