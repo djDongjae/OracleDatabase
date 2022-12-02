1683752fc06734a4b55d7ce2b795bdc3ae0c54cc
Dong-Jae Oh <isf1999@naver.com>
2022-12-02 19:36:28 +0900

    sql2차강의실습 2회독 완성

--- SQL기초2차강의실습_201914089_2ndReading.sql
+++ SQL기초2차강의실습_201914089_2ndReading.sql
@@ -101,14 +101,41 @@ where job_id in (
     where job_title like '%Purchasing%'
 );
 
-/*
-최고의 급여를 받고 있는 사원의 first_name을 출력하라.
-사원의 수가 5명 이상인 부서의 부서 이름을 출력하라.
-업무 이름에 Purchasing이 들어간 업무를 하고 있는 사원의 first_name을 출력하라.
-소속 사원이 없는 부서의 이름을 출력하라.
+--4. 소속 사원이 없는 부서의 이름을 출력하라.
+select department_name
+from departments
+where department_id not in (
+    select distinct department_id
+    from employees
+);
 
-(집합연산)
-배당된 사원이 없는 업무의 이름(job_title)을 출력하라.
-Shipping 부서와 Human Resources 부서의 부서 이름과 주소(address)를 출력하라. 집합연산을 이용한다.
-IT 부서와 Finance 부서 둘 다가 같이 위치한 Country_ID를 출력하라. 집합연산을 이용한다.
-*/
+--(집합 연산)
+
+--1. 배당된 사원이 없는 업무의 이름(job_title)을 출력하라.
+select job_title
+from jobs
+minus
+select job_title
+from jobs
+where job_id in (
+    select job_id
+    from employees
+);
+
+--2. Shipping 부서와 Human Resources 부서의 부서 이름과 주소(address)를 출력하라. 집합연산을 이용한다.
+select d.department_name, l.street_address
+from departments d, locations l
+where d.department_name = 'Shipping'
+union
+select d.department_name, l.street_address
+from departments d, locations l
+where d.department_name = 'Human Resources';
+
+--3. IT 부서와 Finance 부서 둘 다가 같이 위치한 Country_ID를 출력하라. 집합연산을 이용한다.
+select l.country_id
+from departments d, locations l
+where d.department_name = 'IT'
+intersect
+select l.country_id
+from departments d, locations l
+where d.department_name = 'Finance';
